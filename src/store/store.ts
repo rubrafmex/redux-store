8cf0d5059fbd2ec530ef75169f14dd58a711199b
Ruben Rafael <ruben.rafaelmartinez@theapsgroup.com>
2020-05-27 16:19:04 +0200

    added my comments

--- src/store/store.ts
+++ src/store/store.ts
@@ -18,7 +18,11 @@ export class Store {
 
   private reduce(state, action) {
     const newState = {};
+    // iterate in our reducers to bind new state
     for (const prop in this.reducers) {
+      // newState[prop] = this.reducers[prop](state[prop], action); is doing:
+      // newState.todos = this.reducers.todos(state.todos, action);
+      // Because each reducer manages its own piece of state, we use this.reducers[prop] and we pass only state[prop]
       newState[prop] = this.reducers[prop](state[prop], action);
     }
     return newState;
